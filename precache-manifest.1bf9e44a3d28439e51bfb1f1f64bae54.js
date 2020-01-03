db9fa39094223f733dbaddd12013bb24df267be1
lephuhao24 <32507309+lephuhao24@users.noreply.github.com>
2020-01-03 23:15:06 +0700

    Updates

---
+++ precache-manifest.1bf9e44a3d28439e51bfb1f1f64bae54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69345273d0506d2069275b120704edd",
+    "url": "/monsters-users/index.html"
+  },
+  {
+    "revision": "1fe0e0a4aacdcaba6c86",
+    "url": "/monsters-users/static/css/main.05fbe4b6.chunk.css"
+  },
+  {
+    "revision": "cb6e0afd9c40f1aaf308",
+    "url": "/monsters-users/static/js/2.16aafff0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-users/static/js/2.16aafff0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fe0e0a4aacdcaba6c86",
+    "url": "/monsters-users/static/js/main.2f21ad52.chunk.js"
+  },
+  {
+    "revision": "2cf1ae1a2c0fa0a5e8d8",
+    "url": "/monsters-users/static/js/runtime-main.0c4023bf.js"
+  }
+]);
